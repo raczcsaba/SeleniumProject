fae7ee28ed57d7aa8936ac4e664cbbf78e298aa2
Racz Csaba <legoscsabi@gmail.com>
2023-01-21 13:33:26 +0100

    auto accept cookies

--- tests/test1.js
+++ tests/test1.js
@@ -1,17 +1,34 @@
 const { By, Key, Builder, Browser, until, Button } = require('selenium-webdriver')
 require('chromedriver')
 
+function getPlayer(driver){
+    driver.wait(until.elementLocated(By.xpath('//ytd-player'))).then( el => {
+        el.click().then(() => {
+            console.log('player Click')
+            return el
+        })
+    });
+
+}
+
+async function acceptTerms(driver){
+    await driver.wait(until.elementLocated(By.xpath('//button[@aria-label="Accept the use of cookies and other data for the purposes described"]'))).then( el => {
+        console.log("Terms loaded")
+        el.click()
+    });
+}
+
 async function test_case_one(){
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
     try {
         await driver.get('https://www.youtube.com/watch?v=fzBU-tcXt3U')
-        await driver.wait(until.elementLocated(By.xpath('//button/div/span[contains(text(),"Accept all")]')),1000).then(() => {
-            console.log("Terms loaded")
-        });
-        await driver.findElement(By.xpath('//button[@aria-label="Accept the use of cookies and other data for the purposes described"]')).click().then(() => {
-            console.log("Terms Accpeted")
-        })
-        await driver.findElement(By.id('player')).click()
+
+        await acceptTerms(driver)
+        console.log("Terms Accepted")
+
+        let player = await getPlayer(driver)
+        console.log('player')
+
 
 
     }catch (e) {
@@ -20,6 +37,7 @@ async function test_case_one(){
         console.log(' Error end ')
 
     }finally {
+        console.log('Finished')
         await driver.quit
     }
 
